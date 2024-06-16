8591b249fcfc6c6fa5eb81b965879f94c8da3487
Jason Malefakis <anskyny10@aol.com>
2024-06-16 00:29:50 -0400

    add calc functions

---
+++ index.js
@@ -0,0 +1,39 @@
+//test 1
+function add(a, b) {
+    return a + b;
+}
+
+//test 2
+function subtract(a, b) {
+    return a - b;
+}
+
+//test 3
+function multiply(a, b) {
+    return a * b;
+}
+
+//test 4
+function divide(a, b) {
+    return a / b;
+}
+
+//test 5
+function increment(n) {
+    return n += 1;
+}
+
+//test 6
+function decrement(n) {
+    return n -= 1;
+}
+
+//test 7
+function makeInt(string) {
+    return parseInt(string, 10);
+}
+
+//test 8
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
